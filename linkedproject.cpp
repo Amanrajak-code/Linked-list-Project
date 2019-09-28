61e0840a6da953f17434832cfac27108645cc27d
mayanksoni2020 <48762957+mayanksoni2020@users.noreply.github.com>
2019-09-28 23:20:23 +0530

    implemented insert function
    add another function for improving

---
+++ linkedproject.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+int data;
+node *link
+};
+class list{
+private:
+node *head,*tail;
+public:
+list()
+{
+head=NULL;
+tail=NULL;
+}
+void insert(int n){
+node *temp=new node();
+temp->data=n;
+temp->link=NULL;
+while(head==NULL){
+head=temp;
+tail=temp;
+}
+tail=temp;
+tail->link=NULL;
+}
+};
+int main(){
+list obj;
+obj.insert(15);
+}
